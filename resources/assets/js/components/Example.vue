79e62956128c16fae0e916d282feeb3b21e51561
Tony Messias <tonyzrp@gmail.com>
2016-08-04 22:45:48 -0300

    initial commit

---
+++ resources/assets/js/components/Example.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-3">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Menu</div>
+
+                    <div class="panel-body">
+                        <ul class="list-group">
+                            <li class="list-group-item">Item 1</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-9">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Mapa</div>
+
+                    <div class="panel-body">
+                        <map
+                            style="width: 100%; height: 450px; display: block;"
+                            :center="{lat: -9.650896, lng: -35.705712}"
+                            :zoom="12"
+                        >
+                            <marker
+                                v-for="m in markers"
+                                :position="m.position"
+                                :clickable="true"
+                                :draggable="false"
+                                @g-click="m.opened=true"
+                            >
+                                <info-window
+                                    :content="m.info"
+                                    :opened.sync="m.opened"
+                                    :position.sync="m.position"
+                                >
+                                </info-window>
+                            </marker>
+                        </map>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                markers: []
+            }
+        },
+        ready() {
+            const conn = this.$http.get('/places.json');
+
+            conn.then(({data}) => {
+                this.markers = JSON.parse(data);
+            });
+        }
+    }
+</script>
